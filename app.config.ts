f6e25d75bf8245f1c1b893524e27238c1a2000a6
Mneloapp <Mneloapp@users.noreply.github.com>
2026-09-13 14:27:54 +0400

    chore: prepare TestFlight build 12

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,7 @@ const config: ExpoConfig = {
   ios: {
     bundleIdentifier: 'com.mnelo.messenger',
     appleTeamId: 'CS6GJ2BMS9',
-    buildNumber: '11',
+    buildNumber: '12',
     supportsTablet: false,
     associatedDomains: ['applinks:mnelo.com', 'applinks:www.mnelo.com'],
     infoPlist: { UIBackgroundModes: ['audio', 'voip', 'remote-notification'] },
